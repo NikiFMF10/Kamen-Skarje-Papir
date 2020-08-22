eeae3e5487c0c3e15c3c782977a5887bb77bced6
NikiFMF10 <49947147+NikiFMF10@users.noreply.github.com>
2020-08-22 17:01:59 +0200

    Spremenjen model

--- model.py
+++ model.py
@@ -1,14 +1,16 @@
 import random
 
-class KamenSkarjePapir:
+class Igra:
 
     def __init__(self):
         self.izbira_racunalnika = ''
         self.izbira = ['Kamen', 'Škarje', 'Papir']
 
+
     def kaj_izbere_racunalnik (self):
         self.izbira_racunalnika = self.izbira[random.randint(0,2)]
         return self.izbira_racunalnika
+
 
     def igralec (self, izbira, racunalnik):
         if izbira == 'Kamen' and racunalnik == 'Škarje':
@@ -25,3 +27,37 @@ class KamenSkarjePapir:
             return ('Izgubil si. Poskusi ponovno!')
         else:
             return ('Neodločeno. Naslednja igra je tvoja!')
+
+
+class KamenSkarjePapir:
+    
+    def __init__(self):
+        self.igra = {}
+
+
+    def racunalnik_izbere():
+    igra = model.Igra()
+    rac = igra.kaj_izbere_racunalnik()
+    return rac
+
+
+    def zmagovalec (izbira, racunalnik):
+    igra = model.Igra()
+    zmag = igra.igralec(izbira, racunalnik)
+    return zmag
+
+
+    def stevilo_zmag (self, izbira, racunalnik):
+    igra = model.Igra()
+    udelezenec = igra.igralec(izbira, racunalnik)
+    if udelezenec == 'Zmagal si!':
+        self.stevilo_zmag_igralca += 1
+    elif udelezenec == 'Izgubil si. Poskusi ponovno!':
+        self.stevilo_zmag_racunalnika += 1
+    else:
+        self.stevilo_zmag_igralca += 0
+        self.stevilo_zmag_racunalnika += 0
+
+    
+    self.tekst1.config(tekst = 'Igralec je zmagal' + str(self.stevilo_zmag_igralca) + 'krat.')
+    self.tekst2.config(tekst = 'Računalnik je zmagal' + str(self.stevilo_zmag_racunalnika) + 'krat.')
--- spletni vmesnik.py
+++ spletni vmesnik.py
@@ -1,28 +1,6 @@
 import bottle
 import model
 
-def racunalnik_izbere():
-    igra = model.KamenSkarjePapir()
-    rac = igra.kaj_izbere_racunalnik()
-    return rac
-
-def zmagovalec (izbira, racunalnik):
-    igra = model.KamenSkarjePapir()
-    zmag = igra.igralec(izbira, racunalnik)
-    return zmag
-
-def stevilo_zmag (self, izbira, racunalnik):
-    igra = model.KamenSkarjePapir()
-    udelezenec = igra.igralec(izbira, racunalnik)
-    if udelezenec == 'Zmagal si!':
-        self.stevilo_zmag_igralca += 1
-    elif udelezenec == 'Izgubil si. Poskusi ponovno!':
-        self.stevilo_zmag_racunalnika += 1
-    else:
-        self.stevilo_zmag_igralca += 0
-        self.stevilo_zmag_racunalnika += 0
-
-
 @bottle.get('/')
 def index():
     return '<h1>Igra: Kamen, Škarje, Papir</h1>'
