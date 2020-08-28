1864a80b857562daa586f413cba16f5fe2831abe
NikiFMF10 <49947147+NikiFMF10@users.noreply.github.com>
2020-08-28 12:40:41 +0200

    Spremenjen spletni vmesnik

--- spletni vmesnik.py
+++ spletni vmesnik.py
@@ -1,9 +1,32 @@
 import bottle
 import model
 
+model = model.KamenSkarjePapir()
 
 @bottle.get('/')
 def index():
-    return '<h1>Igra: Kamen, Škarje, Papir</h1>'
+    return bottle.template('kamškapap.tpl')
+
+@bottle.post('/igra/')
+def nova_igra():
+    id_igre = model.nova_igra()
+    bottle.redirect('/igra/{0}/'.format(id_igre))
+
+@bottle.get('/igra/<id_igre:int>/')
+def pokazi_igro(id_igre):
+    return bottle.template('igra.tpl',
+    igra = model.igre[id_igre][0],
+    id_igre = id_igre,
+    poskus = model.igre[id_igre][1])
+
+@bottle.post('/igra/<id_igre:int>/')
+def igralec(id_igre):
+    izbira = bottle.request.forms.getunicode("beseda")
+    model.igralec(id_igre, izbira)
+    bottle.redirect('/igra/{0}/'.format(id_igre))
+
+@bottle.get('/img/<picture>')
+def serve_pictures(picture):
+    return bottle.static_file(picture, root = 'img')
 
 bottle.run(reloader=True, debug=True)
